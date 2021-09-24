ac7276d4ebc52e08581263cdb5a87dda0270cf98
Haris Khan <haris.wpbrigade@gmail.com>
2021-09-23 20:07:13 -0700

    init v2

---
+++ components/Thumbnail.js
@@ -0,0 +1,5 @@
+function Thumbnail() {
+  return <div></div>;
+}
+
+export default Thumbnail;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
-import Results from '../components/Results'
-import requests from '../utils/requests'
-import request from '../utils/requests'
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+import request from "../utils/requests";
 
 export default function Home(props) {
-  console.log(props)
+  console.log(props);
   return (
     <div>
       <Head>
@@ -19,19 +19,22 @@ export default function Home(props) {
       <Header />
       <Nav />
       <Results />
-
     </div>
-  )
+  );
 }
 
-export async function getServerSideProps(context){
-  const genre = context.query.genre
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
 
-  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json())
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
 
-  return{
+  return {
     props: {
       results: request.results,
-    }
-  }
+    },
+  };
 }
